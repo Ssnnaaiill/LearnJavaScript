478ddcaf80aabd362f70a13f94f6f79eeb2f2293
Ssnnaaiill <newdk3025@gmail.com>
2019-07-16 09:52:13 +0900

    mail-me

---
+++ mail-me/index.js
@@ -0,0 +1,14 @@
+var modal = document.querySelector('.modal');
+var trigger = document.querySelector('.trigger');
+var closeButton = document.querySelector('.close-button');
+var cancelButton = document.querySelector('#cancel');
+//console.log(modal);
+
+function toggleModal() { modal.classList.toggle("show-modal"); }
+
+function windowOnClick() { if(event.target === modal) { toggleModal(); }}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+cancelButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
